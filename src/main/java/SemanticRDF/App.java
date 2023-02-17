11f10bac00baf041bc9c1558a7970e89ad3906e3
Daniel <danthreepoint@gmail.com>
2023-02-16 16:14:25 -0800

    initia java files

---
+++ src/main/java/SemanticRDF/App.java
@@ -0,0 +1,13 @@
+package SemanticRDF;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
