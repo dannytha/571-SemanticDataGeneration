fc8b1513d351707195ba4b3652e43191f32f603f
dillonhl <dillonhl@uci.edu>
2023-02-26 23:15:58 -0800

    Update App.java
    forgot to update the schema file name in the main function

--- src/main/java/SemanticRDF/App.java
+++ src/main/java/SemanticRDF/App.java
@@ -9,7 +9,7 @@ public class App
        SemanticConverter lax = new SemanticConverter("schema/LAX_PassengerCountByCarrierType_Schema.rdf",
         "dataset/Los_Angeles_International_Airport_Passenger_Count_By_Carrier_Type.csv");
         
-        SemanticConverter covid = new SemanticConverter("schema/LACity_Covid.rdf","dataset/LA_County_COVID_Cases.csv");
+        SemanticConverter covid = new SemanticConverter("schema/LACity_Covid_Schema.rdf","dataset/LA_County_COVID_Cases.csv");
         
         covid.csv_to_rdf();
         lax.csv_to_rdf();
